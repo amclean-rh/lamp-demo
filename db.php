550cf03da5cd5447d0d0fcd358014826020a126a
Adam McLean <amclean@redhat.com>
2016-01-27 13:28:13 -0500

    updated db connection data

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?
-$dbhost = getenv("MYSQL01_SERVICE_HOST");
-$dbport = getenv("MYSQL01_SERVICE_PORT");
-$dbuser = 'mberube';
-$dbpwd = 'toriamos';
-$dbname = 'mysql01';
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = 'mysqluser';
+$dbpwd = 'redhat';
+$dbname = 'sampledb';
 ?>
