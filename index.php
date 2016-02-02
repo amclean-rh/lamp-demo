50763d5720f5f55d927bbc0b157214cf68d45775
Adam McLean <amclean@redhat.com>
2016-02-02 12:46:31 -0500

    added new index content

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <CENTER>
 <img src="redhat.jpg">
 <BR><BR>
-<font size=7 color="red"><b>Hello!</b> We hope you like this demo.</font>
+<font size=7 color="red"><b>Hello this message is new!</b> We hope you like this demo.</font>
 <BR>
 <H4><a HREF='dbtest.php'>Link to a database test...</a></H4>
 </CENTER>
