9bcac3c46cd55b105da8efff76c569ad4a5b5c41
Adam McLean <amclean@redhat.com>
2016-04-26 16:20:06 -0400

    fixed index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <CENTER>
 <img src="redhat.jpg">
 <BR><BR>
-<font size=7 color="red"><b>Welcome to 'The Next Big Thing'</b></font>
+<font size=7 color="red"><b>Hello world!</b></font>
 <BR>
 <H4><a HREF='dbtest.php'>Link to a database test...</a></H4>
 </CENTER>
